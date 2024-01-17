660dbb8d4a1b1e3dc19b6f202693facb1f23d80a
Jacob K <jacobkasper@gmail.com>
2024-01-17 11:42:04 +0000

    updated metier model
    updated the metier model to address misspecification in box effort (had active box coded as boundary box). adjusted catchability accordingly

--- T202066105met/RunAtlantis.sh
+++ T202066105met/RunAtlantis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 find /app/model -type f | xargs dos2unix
-atlantisMerged -i InitialCond92.nc 0 -o Out.nc -r Run.prm -f Force1d_5metbox.prm -p Physics.prm -b B391ES3JK5.5e.prm -s GroupsIceland.csv -q FisheriesIceland3.csv -h F155ES18JK11.14.prm -d output
+atlantisMerged -i InitialCond92.nc 0 -o Out.nc -r Run.prm -f Force3.prm -p Physics.prm -b B391ES3JK5.5e.prm -s GroupsIceland.csv -q FisheriesIceland3.csv -h F155ES18JK11.23.prm -d output
 
 #InitialCond92.nc 
